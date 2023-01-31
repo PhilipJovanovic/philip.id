cc98614cd266b9d6d6930f5503b7a89fabc4c4a3
Philip Jovanovic <philip119@gmx.de>
2023-01-31 23:29:32 +0100

    update + tcard

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Philip J';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'philip';
+export const SITE_DESCRIPTION = 'fullstack developer 🦆';
