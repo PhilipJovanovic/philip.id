70bbefa4c3cfed44f046ce6269ca153226a3b943
Philip Jovanovic <philip119@gmx.de>
2023-01-31 23:29:46 +0100

    ups

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'philip';
-export const SITE_DESCRIPTION = 'fullstack developer 🦆';
+export const SITE_DESCRIPTION = 'full stack developer 🦆';
